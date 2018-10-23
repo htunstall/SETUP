f47243028924f608745ec3a05c21e226156f4a38
Harry Tunstall <H.Tunstall@warwick.ac.uk>
2018-10-23 16:06:43 +0100

    Updated move-data.sh for SiC-testing-framework

--- bin/move-data.sh
+++ bin/move-data.sh
@@ -1,65 +1,83 @@
 #!/bin/bash
-usage="${0##*/} [dir_name] [-h] -- Where [dir_name] will be created in the below folders and all test data will be moved to the sub-folder in raw-data:
+usage="${0##*/} [options] -- Moves all non-default files to the raw-data directory:
+   Options:
+     -h        Displays this help
+     -d        Deletes all non-default files in the directory (Takes precedence over the -o arg)
+     -o        Move data with overwrite
+     -f        Force overwrite without prompting"
+git_repo_path="/storage/chem/msufgx/postgrad/software/SiC-framework"
 
-   'SiC-framework/raw-data/[dir_name]' and
-   'SiC-framework/analysis/output/graphs/[dir_name]'"
-username="msufgx"
-git_repo_path="/storage/chem/$username/postgrad/software/SiC-framework"
+# Default to not delete files in the directory
+delete=False
+overwrite=False
+force=False
 
-while getopts 'h' option; do
+# Get options
+while getopts 'hdof' option; do
    case "$option" in
       h) echo "$usage"
          exit
          ;;
-     \?) echo "$usage"
+      d) delete=True
+         ;;
+      o) overwrite=True
+         ;;
+      f) force=True
+         ;;
+     \?) echo "Invalid Option: -$OPTARG"
          exit 1
          ;;
    esac
 done
 
-# Are we given the correct arguments?
-if [ -z "$1" ]; then
-   echo "$usage"
-else
-   data_path="$git_repo_path/raw-data/$1"
-   out_path="$git_repo_path/analysis/outputs/graphs/$1"
+# Initialise variables
+data_path="$git_repo_path/raw-data"
+cwd=$(pwd)
+basename=${cwd##*/}
 
-   cwd=$(pwd)
-   basename=${cwd##*/}
-   # If we're in the correct directory
-   if [ "$basename" == "run_dir" ]; then
-      # If the dir we want to create exists then append "_1" to both paths
-      #   and the input dirname variable
-      append="False"
-      if [ -d "$data_path" ]; then
-         append="True"
-         data_path="${data_path}_1"
-         out_path="${out_path}_1"
-      fi
-      # Make the two output directories
-      mkdir $data_path
-      mkdir $out_path
+# If we're in the correct directory
+if [ "$basename" == "run_dir" ]; then
+   # If the hidden directory doesn't exist make it
+   if [ ! -d "$cwd/.default-files" ]; then
+      mkdir .default-files
+   fi
+   # Move the default files to a safe directory
+   mv default_* .default-files
+   mv test.bugs_script* .default-files
 
-      # If the hidden directory doesn't exist make it
-      if [ ! -d "$cwd/.default-files" ]; then
-         mkdir .default-files
-      fi
-
-      # Move the default files to a safe directory
-      mv default_* .default-files
-      mv test.bugs_script* .default-files
-
-      # Move the rest of the files to the new dir
-      mv * $data_path
-
-      # Move the default files back here
-      mv .default-files/* ./
-      if [ "$append" == "False" ]; then
-         echo "Data files moved to '/raw-data/$1'"
+   if [ "$delete" == True ]; then
+      if [ "$force" == True ]; then
+         rm -f *
       else
-         echo "Data files moved to '/raw-data/${1}_1'"
+         rm -i *
       fi
    else
-      echo "$usage"$'\n\033[31m\e[1mYou are not in the testing directory! Exiting.\e[0m'
+      # For each *.json file in the testing directory
+      for filename in ./*.json; do
+         # If the json file exists
+         if [[ (-f $data_path/$filename) && "$overwrite" == False ]]; then
+            echo "Data relating to '$filename' not moved as it already exists in raw-data. If you wish to overwrite data please use -o"
+         else
+            # Cut of run_ by removeing upto the first "_" character
+            general_filename=${filename#*/}
+            # Cut everythin after the last "-" character
+            general_filename=${general_filename%-*}
+
+            if [[ ("$overwrite" == True) && "$force" == True ]]; then
+               mv -f *$general_filename* $data_path
+            else
+               mv -i *$general_filename* $data_path
+            fi
+
+            echo "Data files for '*$general_filename*' moved to '/raw-data'"
+         fi
+      done
    fi
+
+   # Move the default files back here
+   mv .default-files/* ./
+
+# If the user is not in "run_dir"
+else
+   echo "$usage"$'\n\n\033[31m\e[1mYou are not in the testing directory! Exiting.\e[0m'
 fi
