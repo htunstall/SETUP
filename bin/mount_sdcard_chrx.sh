df7d684a92e9a54d2a91f59ad7f51d5504c827be
Harry Tunstall <harry.tunstall@googlemail.com>
2018-12-14 19:30:18 +0000

    Now uses ext4 formated sdcard, not exFAT

--- bin/mount_sdcard_chrx.sh
+++ bin/mount_sdcard_chrx.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "Unmounting SD Card"
-sudo umount /media/sdcard
+
+# If the mountpoint is not mounted
+mountpoint="/media/sdcard"
+if [ "$(df | grep $mountpoint)" ]; then
+  echo "Unmounting SD Card"
+  sudo umount /media/sdcard
+else
+  echo "SD Card is not already mounted!"
+fi
 
 echo "Mounting SD Card with write access"
-sudo mount -t vfat -o rw,uid=1000 /dev/mmcblk1p1 /media/sdcard
+sudo mount -t ext4 /dev/mmcblk1p1 /media/sdcard
+
+# Make usre it's owned by the user
+sudo chown -R $USER:$USER /media/sdcard
