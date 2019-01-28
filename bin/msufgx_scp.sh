ca4300f75060ceb18d74919d6eb45deb9924a36f
tunstall <tunstall@login02.cluster>
2019-01-28 20:57:09 +0000

    updated athena alias', removed unnessacary scripts

--- bin/msufgx_scp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$1" ] && [ "$2" ]; then
-    scp -r msufgx@barbagallo.csc.warwick.ac.uk:${1} ${2}
-else
-    echo "You need to specify a source and destination as \$1 and \$2!"
-fi
